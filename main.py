bf6bf80f11392daa179b6c21e151afe05636021d
Jonasz-D <jdanisz@interia.pl>
2024-01-14 11:29:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,8 @@
 
-
-def main():
-    flag = True
-    while flag:
-        command = input('Write your command: ').lower().strip().split()
-
-        try:
-            base_command = command[0]
-        except IndexError:
-            continue
-
-        handler = handler_command(base_command, command)
-
-        if isinstance(handler, str):
-            print(handler)
-        
-        elif isinstance(handler, bool):
-            flag = handler
-
-        else:
-            handler
-
-
 def input_error(func):
     def inner(base_command, command):
         try:
-            func(base_command, command)
+            return func(base_command, command)
         except KeyError:
             if base_command == 'change' :
                 return 'The name is unknown'
@@ -41,8 +18,6 @@ def input_error(func):
             else:
                 return
 
-        else:
-            return func(base_command, command)
     return inner
 
 
@@ -50,17 +25,18 @@ def hello(command):
     return 'How can I help you?'
 
 def add(command):
-    name = command[1]
-    phone_num = command[2]
+    name, phone_num = command[1], command[2]
 
-    if not phone_num.isdigit():
+    if name in contacts:
+        return 'The contact already exists'
+    elif not phone_num.isdigit():
         raise ValueError
     else:
-        contacts[name] = phone_num
+        return contacts.update({name:phone_num})
 
 def change(command):
-    name = command[1]
-    phone_num = command[2]
+    name, phone_num = command[1], command[2]
+    
     if name in contacts:
         contacts[name] = phone_num
 
@@ -100,5 +76,26 @@ OPERATIONS = {
 def handler_command(base_command, command):
     return OPERATIONS[base_command](command)
 
+def main():
+    flag = True
+    while flag:
+        command = input('Write your command: ').lower().strip().split()
 
-main()
+        try:
+            base_command = command[0]
+        except IndexError:
+            continue
+
+        handler = handler_command(base_command, command)
+
+        if isinstance(handler, str):
+            print(handler)
+        
+        elif isinstance(handler, bool):
+            flag = handler
+
+        else:
+            handler
+
+if __name__ == '__main__':
+    main()
